4f52d5700698cd00b2f3e67354feb5ebbb9dce00
gabrieldrouin <gabdrouin8@gmail.com>
2024-04-30 16:35:37 -0400

    Send message to discord if bot stops working

--- app.js
+++ app.js
@@ -16,30 +16,35 @@ const { URL, refreshTime } = require('./config');
     console.log("A new Genote tab will open. Do not close it.");
     discordBot.sendMessage("Bot started.");
 
-    async function compareTableContents() {
-        const elements = await util.getClasses(page, URL);
-        elements.forEach((element, index) => {
-            curr[index] = element;
-            const className = util.getClassName(element);
-            
-            if (!className) return;
-            if (prev[index] === curr[index]) {
-                console.log(`No difference: ${className}`);
-            } else if (prev[index]) {
-                console.log(`Difference: ${className}`);
-                discordBot.sendMessage(`New grade published for class: ${className}`);
-            }
-            prev[index] = curr[index];
-        });
-    }
+    try {
+        async function compareTableContents() {
+            const elements = await util.getClasses(page, URL);
+            elements.forEach((element, index) => {
+                curr[index] = element;
+                const className = util.getClassName(element);
 
-    async function checkForDifferences() {
-        while (true) {
-            await compareTableContents();
-            await new Promise(resolve => setTimeout(resolve, refreshTime)); 
+                if (!className) return;
+                if (prev[index] === curr[index]) {
+                    console.log(`No difference: ${className}`);
+                } else if (prev[index]) {
+                    console.log(`Difference: ${className}`);
+                    discordBot.sendMessage(`New grade published for class: ${className}`);
+                }
+                prev[index] = curr[index];
+            });
         }
-    }
 
-    await checkForDifferences();
+        async function checkForDifferences() {
+            while (true) {
+                await compareTableContents();
+                await new Promise(resolve => setTimeout(resolve, refreshTime)); 
+            }
+        }
+
+        await checkForDifferences();
+    } finally {
+        console.log("Notifying bot before exit...");
+        discordBot.sendMessage("Bot stopped.");
+    }
 })();
 
